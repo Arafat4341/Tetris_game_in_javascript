c729b9df0924f59496c6b7666515c4c5e15c697f
Arafat4341 <mdyasinarafatyen2@gmail.com>
2018-12-22 22:28:30 -0800

    Enabled keyboard input to move the T piece

--- tetris.js
+++ tetris.js
@@ -26,7 +26,12 @@ function drawMatrix(matrix, offset){
 			}
 		});
 	});
-};
+}
+
+function playerDrop(){
+	player.pos.y++;
+	dropCounter = 0;
+}
 
 let dropCounter = 0;
 let dropInterval = 1000;
@@ -37,8 +42,7 @@ function update(time = 0){
 	lastTime = time;
 	dropCounter += deltaTime;
 	if(dropCounter > dropInterval){
-		player.pos.y++;
-		dropCounter = 0;
+		playerDrop();
 	}
 	draw();
 	requestAnimationFrame(update);
@@ -48,5 +52,15 @@ const player = {
 	pos : {x: 5, y: 5},
 	matrix: matrix
 }
+
+document.addEventListener('keydown', event => {
+	if(event.keyCode === 37){
+		player.pos.x--;
+	} else if(event.keyCode === 39){
+		player.pos.x++;
+	} else if(event.keyCode === 40){
+		playerDrop();
+	}
+});
 
 update();
