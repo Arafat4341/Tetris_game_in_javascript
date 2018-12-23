51241cae2459864af718315cfa14c2ffb0ce6c1a
Arafat4341 <mdyasinarafatyen2@gmail.com>
2018-12-23 08:07:37 -0800

    enabled collide function and made bound for the pieces

--- tetris.js
+++ tetris.js
@@ -1,8 +1,6 @@
 const canvas = document.getElementById('tetris');
 const context = canvas.getContext('2d');
 
-
-
 context.scale(20, 20);
 
 const matrix = [
@@ -11,16 +9,39 @@ const matrix = [
 	[0, 1, 0]
 ];
 
+function collide(arena, player){
+	const m = player.matrix;
+	const o = player.pos;
+	for (let y = 0; y < m.length; ++y) {
+		for (let x = 0; x < m[y].length; ++x) {
+			if(m[y][x] !== 0 && (arena[y + o.y] && arena[y + o.y][x + o.x]) !== 0){
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+function createMatrix(w, h){
+	const matrix = [];
+	while(h--){
+		matrix.push(new Array(w).fill(0));
+	}
+	return matrix;
+}
+
 function draw(){
 	context.fillStyle = '#000000';
 	context.fillRect(0, 0, canvas.width, canvas.height);
+
+	drawMatrix(arena, {x:0, y:0});
 	drawMatrix(player.matrix, player.pos);	
 }
 
 function drawMatrix(matrix, offset){
 	matrix.forEach((row, y) => {
 		row.forEach((value, x) => {
-			if (value != 0) {
+			if (value !== 0) {
 				context.fillStyle = 'red';
 				context.fillRect(x + offset.x, y + offset.y, 1, 1);
 			}
@@ -28,9 +49,31 @@ function drawMatrix(matrix, offset){
 	});
 }
 
+function merge(arena, player){
+	player.matrix.forEach((row, y) => {
+		row.forEach((value, x) => {
+			if(value !== 0){
+				arena[y + player.pos.y][x + player.pos.x] = value;
+			}
+		});
+	});
+}
+
 function playerDrop(){
 	player.pos.y++;
+	if(collide(arena, player)){
+		player.pos.y--;
+		merge(arena, player);
+		player.pos.y = 0;
+	}
 	dropCounter = 0;
+}
+
+function playerMove(dir){
+	player.pos.x += dir;
+	if(collide(arena, player)){
+		player.pos.x -= dir;
+	}
 }
 
 let dropCounter = 0;
@@ -48,6 +91,8 @@ function update(time = 0){
 	requestAnimationFrame(update);
 }
 
+const arena = createMatrix(12, 20);
+
 const player = {
 	pos : {x: 5, y: 5},
 	matrix: matrix
@@ -55,9 +100,9 @@ const player = {
 
 document.addEventListener('keydown', event => {
 	if(event.keyCode === 37){
-		player.pos.x--;
+		playerMove(-1);
 	} else if(event.keyCode === 39){
-		player.pos.x++;
+		playerMove(1);
 	} else if(event.keyCode === 40){
 		playerDrop();
 	}
