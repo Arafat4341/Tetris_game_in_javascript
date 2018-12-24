22f2e071f36dff9a75ea321974d95307c6ea5115
Arafat4341 <mdyasinarafatyen2@gmail.com>
2018-12-23 22:46:08 -0800

    added scoring functionality

--- tetris.js
+++ tetris.js
@@ -4,6 +4,7 @@ const context = canvas.getContext('2d');
 context.scale(20, 20);
 
 function arenaSweep(){
+	let rowCount = 1;
 	outer: for(let y = arena.length - 1; y > 0; --y){
 		for(let x = 0; x < arena[y].length; ++x){
 			if(arena[y][x] === 0){
@@ -13,6 +14,9 @@ function arenaSweep(){
 		const row = arena.splice(y, 1)[0].fill(0);
 		arena.unshift(row);
 		++y;
+
+		player.score += rowCount*10;
+		rowCount *= 2;
 	}
 }
 
@@ -119,6 +123,7 @@ function playerDrop(){
 		merge(arena, player);
 		playerReset();
 		arenaSweep();
+		updateScore();
 	}
 	dropCounter = 0;
 }
@@ -137,6 +142,8 @@ function playerReset() {
 	player.pos.x = (arena[0].length / 2 | 0) - (player.matrix[0].length / 2 | 0);
 	if(collide(arena, player)){
 		arena.forEach(row => row.fill(0));
+		player.score = 0;
+		updateScore();
 	}
 }
 
@@ -186,6 +193,10 @@ function update(time = 0){
 	requestAnimationFrame(update);
 }
 
+function updateScore(){
+	document.getElementById('score').innerText = player.score;
+}
+
 const arena = createMatrix(12, 20);
 
 const colors = [
@@ -200,8 +211,9 @@ const colors = [
 ];
 
 const player = {
-	pos : {x: 5, y: 5},
-	matrix: createPiece('T')
+	pos : {x: 0, y: 0},
+	matrix: null,
+	score: 0
 }
 
 document.addEventListener('keydown', event => {
@@ -218,4 +230,6 @@ document.addEventListener('keydown', event => {
 	}
 });
 
+playerReset();
+updateScore();
 update();
