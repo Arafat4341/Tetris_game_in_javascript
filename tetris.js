14bbe60beb1e377ff836a59ce0262fcb56035832
Arafat4341 <mdyasinarafatyen2@gmail.com>
2018-12-23 21:00:37 -0800

    added different colors to different pieces

--- tetris.js
+++ tetris.js
@@ -33,39 +33,39 @@ function createPiece(type) {
 		];
 	} else if(type == 'O'){
 		return [
-			[1, 1],
-			[1, 1]
+			[2, 2],
+			[2, 2]
 		];
 	} else if(type == 'L'){
 		return [
-			[0, 1, 0],
-			[0, 1, 0],
-			[0, 1, 1]
+			[0, 3, 0],
+			[0, 3, 0],
+			[0, 3, 3]
 		];
 	} else if(type == 'J'){
 		return [
-			[0, 1, 0],
-			[0, 1, 0],
-			[1, 1, 0]
+			[0, 4, 0],
+			[0, 4, 0],
+			[4, 4, 0]
 		];
 	} else if(type == 'S'){
 		return [
-			[0, 1, 1],
-			[1, 1, 0],
+			[0, 5, 5],
+			[5, 5, 0],
 			[0, 0, 0]
 		];
 	} else if(type == 'Z'){
 		return [
-			[1, 1, 0],
-			[0, 1, 1],
+			[6, 6, 0],
+			[0, 6, 6],
 			[0, 0, 0]
 		];
 	} else if(type == 'I'){
 		return [
-			[0, 1, 0, 0],
-			[0, 1, 0, 0],
-			[0, 1, 0, 0],
-			[0, 1, 0, 0]
+			[0, 7, 0, 0],
+			[0, 7, 0, 0],
+			[0, 7, 0, 0],
+			[0, 7, 0, 0]
 		];
 	}
 }
@@ -82,7 +82,7 @@ function drawMatrix(matrix, offset){
 	matrix.forEach((row, y) => {
 		row.forEach((value, x) => {
 			if (value !== 0) {
-				context.fillStyle = 'red';
+				context.fillStyle = colors[value];
 				context.fillRect(x + offset.x, y + offset.y, 1, 1);
 			}
 		});
@@ -173,6 +173,17 @@ function update(time = 0){
 }
 
 const arena = createMatrix(12, 20);
+
+const colors = [
+    null,
+    '#FF0D72',
+    '#0DC2FF',
+    '#0DFF72',
+    '#F538FF',
+    '#FF8E0D',
+    '#FFE138',
+    '#3877FF',
+];
 
 const player = {
 	pos : {x: 5, y: 5},
